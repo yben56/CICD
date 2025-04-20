7136a9670c477311d1bcb890e5d625a8c6a4b024
DESKTOP-U6EJ3LB\benjamin <knockknockinfo@gmail.com>
2025-04-21 05:32:12 +0800

    add counter.vue

---
+++ src/components/Counter.vue
@@ -0,0 +1,11 @@
+<!-- src/components/Counter.vue -->
+<template>
+  <button @click="count++">Count is: {{ count }}</button>
+</template>
+  
+  <script setup>
+  import { ref } from 'vue'
+  
+  const count = ref(0)
+  </script>
+  
